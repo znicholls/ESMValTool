b72fd3b84e6cef9cd5e1085d450926d4a916367d
Valeriu Predoi <valeriu.predoi@gmail.com>
2018-06-18 11:17:11 +0100

    added fixes for pr variable

--- esmvaltool/cmor/_fixes/CMIP6/GFDL_CM4.py
+++ esmvaltool/cmor/_fixes/CMIP6/GFDL_CM4.py
@@ -32,3 +32,35 @@ class ta(Fix):
         
 
         return cube
+
+
+class pr(Fix):
+    """Fixes for msftmyz"""
+
+    def fix_metadata(self, cube):
+        """
+        Fix data
+
+        Fixes:
+            - reassigns time units from:
+              cf_units.Unit('days since 0001-01-01 00:00:00',
+                            calendar=cube.coord('time').units.calendar)
+              to
+              cf_units.Unit('days since 1850-01-01 00:00:00',
+                            calendar=cube.coord('time').units.calendar)
+
+        Parameters
+        ----------
+        cube: iris.cube.Cube
+
+        Returns
+        -------
+        iris.cube.Cube
+
+        """
+        new_t_units = cf_units.Unit('days since 1850-01-01 00:00:00',
+                                    calendar=cube.coord('time').units.calendar)
+        cube.coord('time').units = new_t_units
+
+
+        return cube
--- esmvaltool/cmor/_fixes/CMIP6/IPSL_CM6A_LR.py
+++ esmvaltool/cmor/_fixes/CMIP6/IPSL_CM6A_LR.py
@@ -35,3 +35,33 @@ class ta(Fix):
         cube.coord('air_pressure').var_name = 'plev'
 
         return cube
+
+class pr(Fix):
+    """Fixes for msftmyz"""
+
+    def fix_metadata(self, cube):
+        """
+        Fix data
+
+        Fixes:
+            - adds 180 deg to longitude points
+            - order latitude points
+            - set air_pressure coord name to plev
+            - set air_pressure variable name to plev
+
+        Parameters
+        ----------
+        cube: iris.cube.Cube
+
+        Returns
+        -------
+        iris.cube.Cube
+
+        """
+        # check for CMOR consistency
+        if np.min(cube.coord('longitude').points) < 0:
+            cube.coord('longitude').points = cube.coord('longitude').points + 180.
+        if cube.coord('latitude').points[0] > cube.coord('latitude').points[-1]:
+            cube.coord('latitude').points = np.sort(cube.coord('latitude').points)
+
+        return cube
