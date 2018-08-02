07661a5ec0e75504bdf1080a4bfb98914839178d
Manuel Schlund <manuel.schlund@dlr.de>
2018-08-02 10:11:02 +0200

    Added unit conversion

--- esmvaltool/diag_scripts/bremen_practicals/example_1.py
+++ esmvaltool/diag_scripts/bremen_practicals/example_1.py
@@ -45,6 +45,8 @@ def main(cfg):
 
     # Process the data
     mmm_cube = mmm_cube.collapsed('time', iris.analysis.MEAN)
+    if mmm_data['short_name'] == 'pr':
+        mmm_cube.convert_units('kg m-2 day-1')
 
     # Plot the data
     iris.quickplot.contourf(mmm_cube)
@@ -69,6 +71,8 @@ def main(cfg):
 
     # Calculate temporal mean and bias
     obs_cube = obs_cube.collapsed('time', iris.analysis.MEAN)
+    if obs_data['short_name'] == 'pr':
+        obs_cube.convert_units('kg m-2 day-1')
     bias_cube = mmm_cube - obs_cube
     bias_cube.rename("Bias in surface air temperature")
 
