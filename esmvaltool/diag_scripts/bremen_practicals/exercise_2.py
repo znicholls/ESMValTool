4cb3e77902eac9e4ef8123a38173aaf67aa1d6ed
Manuel Schlund <manuel.schlund@dlr.de>
2018-08-03 11:49:00 +0200

    Removed exercise 2

---
+++ esmvaltool/diag_scripts/bremen_practicals/exercise_2.py
@@ -0,0 +1,41 @@
+"""Climate Modelling Part 2 (Bremen, 2018): Diagnostic for exercise 2."""
+
+import logging
+import os
+
+import iris
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+from esmvaltool.diag_scripts.shared import run_diagnostic
+
+
+logger = logging.getLogger(os.path.basename(__file__))
+
+
+def main(cfg):
+    """Execute the diagnostic."""
+    plot_path = os.path.join(cfg['plot_dir'], 'exercise_2.png')
+
+    # Read datasets and plot them
+    for (path, attr) in cfg['input_data'].items():
+        logger.info("Reading %s", path)
+        cube = iris.load_cube(path)
+        cube = cube.aggregated_by('year', iris.analysis.MEAN)
+        cube.convert_units('celsius')
+        iris.quickplot.plot(cube.coord('year'), cube, label=attr['dataset'])
+
+    # Add legend and save plot
+    plt.legend()
+    plt.savefig(plot_path)
+    logger.info("Writing %s", plot_path)
+    plt.close()
+
+
+# Call the main function when the script is called
+if __name__ == '__main__':
+
+    with run_diagnostic() as config:
+        main(config)
--- esmvaltool/diag_scripts/bremen_practicals/exercise_3.py
+++ esmvaltool/diag_scripts/bremen_practicals/exercise_3.py
@@ -9,7 +9,8 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-from esmvaltool.diag_scripts.shared import run_diagnostic
+from esmvaltool.diag_scripts.shared import (run_diagnostic, group_metadata,
+                                            select_metadata)
 
 
 logger = logging.getLogger(os.path.basename(__file__))
@@ -19,15 +20,35 @@ def main(cfg):
     """Execute the diagnostic."""
     plot_path = os.path.join(cfg['plot_dir'], 'exercise_3.png')
 
-    # Read datasets and plot them
-    for (path, attr) in cfg['input_data'].items():
-        logger.info("Reading %s", path)
-        cube = iris.load_cube(path)
-        cube = cube.aggregated_by('year', iris.analysis.MEAN)
-        cube.convert_units('celsius')
-        iris.quickplot.plot(cube.coord('year'), cube, label=attr['dataset'])
+    # Group data
+    input_data = cfg['input_data'].values()
+    grouped_input_data = group_metadata(input_data, 'dataset')
+
+    # Iterate over data and subtract historical data from rcp85
+    for (dataset, data) in grouped_input_data.items():
+        hist_data = select_metadata(data, exp='historical')[0]
+        rcp85_data = select_metadata(data, exp='rcp85')[0]
+        hist_file = hist_data['filename']
+        rcp85_file = rcp85_data['filename']
+
+        # Load cubes
+        hist_cube = iris.load_cube(hist_file)
+        rcp85_cube = iris.load_cube(rcp85_file)
+        logger.info("Reading %s", hist_file)
+        logger.info("Reading %s", rcp85_file)
+
+        # Process data
+        hist_cube = hist_cube.collapsed('time', iris.analysis.MEAN)
+        rcp85_cube = rcp85_cube.aggregated_by('year', iris.analysis.MEAN)
+        rcp85_cube = rcp85_cube - hist_cube
+        rcp85_cube.rename('Anomalies in surface air temperature relative to '
+                          '1961-1990')
+
+        # Plot
+        iris.quickplot.plot(rcp85_cube.coord('year'), rcp85_cube, label=dataset)
 
     # Add legend and save plot
+    plt.ylabel('Surface air temperature / K')
     plt.legend()
     plt.savefig(plot_path)
     logger.info("Writing %s", plot_path)
