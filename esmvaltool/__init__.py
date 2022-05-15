03b1669015528d869a2279bb7f4d7de93f93d9a9
Ubuntu <ubuntu@cmip6-crunching.novalocal>
2022-05-15 04:23:19 +0000

    Try adding null handler

--- esmvaltool/__init__.py
+++ esmvaltool/__init__.py
@@ -1,5 +1,9 @@
 """ESMValTool diagnostics package."""
+import logging
 __version__ = '2.5.0'
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
 
 
 class ESMValToolDeprecationWarning(UserWarning):
